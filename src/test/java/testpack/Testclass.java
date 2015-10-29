eb00998b9cce6c15e02c5cd55ce289fdb62bf2d0
Jim Cherian <jim.cherian@tavant.com>
2015-10-29 18:39:53 +0530

    SeleniumTest Version 1

---
+++ src/test/java/testpack/Testclass.java
@@ -0,0 +1,13 @@
+package testpack;
+
+import org.testng.annotations.Test;
+
+public class Testclass {
+	
+	@Test
+	public void testmethod()
+	{
+		System.out.println("This is verison 1 code");
+	}
+
+}
